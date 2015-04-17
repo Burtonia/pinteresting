976fe5efc1327af49f9af64af6b1515d983260d8
Alexander Burton <abrx21@gmail.com>
2015-04-16 22:09:37 -0400

    cleaned up the pins scaffled

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,51 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show # Show Action aka Read
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def edit
+  end
+
+  def create # Create Action
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update # Update Action
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy # Destroy Action
+    @pin.destroy
+      redirect_to pins_url, notice: 'Pin was successfully destroyed.'
+  end
+
+  private
+
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
